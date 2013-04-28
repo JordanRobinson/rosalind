5d8a88203ccc8241bf9dcf8e9fed027db422f1f4
Jordan Robinson <hypahnova@gmail.com>
2013-04-28 22:02:15 +0100

    Adding lcsm.

---
+++ lcsm.rb
@@ -0,0 +1,26 @@
+class LCSM
+#Rosalind exercise: Finding a Shared Motif
+
+
+  def run(data)
+
+    #strip headings
+    raw_array = data.split(/>Rosalind_[0-9].*?\n/)
+
+
+    #there are much better ways of doing this
+    #but for now let's just brute force it, till we have a better understanding of the problem
+
+    for i in 0..raw_array[0].length
+      for j in 0..raw_array.length
+        puts raw_array[j][i]
+      end
+      puts "\n"
+    end
+
+
+
+  end
+
+
+  end
