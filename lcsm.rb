e20135754d0fe841fb5e2457b625a05c1d093134
Jordan Robinson <hypahnova@gmail.com>
2013-12-07 14:44:44 +0000

    A much, much faster approach to lcsn. Also adds comments.

--- lcsm.rb
+++ lcsm.rb
@@ -1,73 +1,60 @@
 class LCSM
 #Rosalind exercise: Finding a Shared Motif
 
-
   def run(data)
 
     #strip headings
     raw_array = data.split(/>Rosalind_[0-9].*?\n/)
 
-    #there are much better ways of doing this
-    #but for now let's just brute force it, till we have a better understanding of the problem
+    all_substrings = []
 
-    longest_substrings = []
+    first_string = raw_array[1]
 
-    for j in 0..raw_array.length
-      unless raw_array[j].nil? or raw_array[j + 1].nil?
-        longest_substrings <<  get_common_substring(raw_array[j], raw_array[j + 1])
-      end
-    end
+    first_string.delete!("\n")
 
-    while longest_substrings.length > 1
-      puts longest_substrings.length
-      master_container = longest_substrings
-      longest_substrings = []
-
-      for j in 0..master_container.length
-        longest_substrings <<  get_common_substring(master_container[j], master_container[j + 1])
-        longest_substrings.delete_at(j)
-        longest_substrings.delete_at(j + 1)
-      end
-
-    end
-
-    puts longest_substrings
-
-  end
-
-  #@param [String] first_string
-  #@param [String] second_string
-  def get_common_substring(first_string, second_string)
-
-    unless second_string.nil?
-      first_string.delete!("\n")
-      second_string.delete!("\n")
-      all_substrings = []
-
-      (0...first_string.length).map {
-          |i| (i...first_string.length).each {
-            |j| if first_string[i..j].length > 1
-                  all_substrings << first_string[i..j]
-                end
-        }
+    (0...first_string.length).map { #grab all the substrings of the first available string
+        |i| (i...first_string.length).each {
+          |j| if first_string[i..j].length > 1
+                all_substrings << first_string[i..j]
+              end
       }
+    }
 
-      puts "---"
+    longest_substring = ""
 
-      longest_substring = ""
+    for j in 2..raw_array.length #for the rest of the strings
+      unless raw_array[j].nil?
 
-      #assuming both strings have the same length
-      for i in 0..all_substrings.length
-        unless all_substrings[i].nil?
-          if second_string.include?(all_substrings[i]) and all_substrings[i].length > longest_substring.length
-            longest_substring = all_substrings[i]
+        raw_array[j].delete!("\n") #strip the newlines
+
+        for i in 0..all_substrings.length #then for every substring of the first string
+          unless all_substrings[i].nil?
+
+            if raw_array[j].include?(all_substrings[i]) and all_substrings[i].length > longest_substring.length
+              longest_substring = all_substrings[i] #check if there's a new common longest substring
+            end
+
+            unless raw_array[j].include?(all_substrings[i]) #remove strings that are not common
+
+              if (all_substrings.length % 1000) == 0 #rudimentary countdown
+                puts all_substrings.length
+              end
+
+              all_substrings.delete_at(i) #the string doesn't match, so remove it
+
+              if all_substrings[i].eql?(longest_substring) #if it was the longest substring, we can't use it any more
+                longest_substring = ""
+                i = 0 #and we need to reset checking all the whole strings
+              end
+            end
+
           end
         end
+
+
       end
-
-      puts longest_substring
-      longest_substring
     end
-  end
+    puts longest_substring
 
+  end
 end
