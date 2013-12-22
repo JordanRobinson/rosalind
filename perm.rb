a4ba37f81ccf50a0386a2af6e50c72480e70dfdc
Jordan Robinson <hypahnova@gmail.com>
2013-12-22 04:04:07 +0000

    Adds solution for perm

--- perm.rb
+++ perm.rb
@@ -2,7 +2,24 @@ class PERM
   #Rosalind exercise: Enumerating gene Orders
 
   def run(data)
-    puts data
+
+    data_as_int = Integer(data)
+
+    numbers = []
+
+    for i in 1..data_as_int
+      numbers.push(i)
+    end
+
+    numbers = numbers.permutation.to_a
+
+    puts numbers.length
+    for i in 0..numbers.length
+      unless numbers[i].nil?
+        puts numbers[i].join(" ")
+      end
+    end
+
   end
 
 end
