58ed75c7fec8f8e56b39c6f8f38990c0cb6aab87
Jordan Robinson <jrobinson.jobs@gmail.com>
2013-12-27 21:08:32 +0000

    More work on LEXF, though triggers ruby bug.

--- lexf.rb
+++ lexf.rb
@@ -1,24 +1,26 @@
 class LEXF
   #Rosalind exercise: Enumerating k-mers Lexicographically
 
-  require 'set'
   def run(data)
 
     raw_array = data.split(/\n/)
 
     letters = raw_array[0].split(/ /)
 
-    letters = letters + letters
+    letters = letters + letters + letters
 
     letters = letters.permutation.to_a
 
-    output = Set.new()
+    output = []
 
     for i in 0..letters.length
       unless letters[i].nil?
-        output.add(letters[i].join(" ")[0] + letters[i].join(" ")[1])
+        output << letters[i].join("")[0..3]
+        #output.add(letters[i].join(" ")[0] + letters[i].join(" ")[1])
       end
     end
+
+    output = output.uniq
 
     output.each do |i|
       puts i
