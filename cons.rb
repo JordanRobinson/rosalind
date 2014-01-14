063bcda5571b0d5a270167fedda1e32d58409eea
Jordan Robinson <hypahnova@gmail.com>
2014-01-14 19:12:42 +0000

    Should solve CONS, can't check though so may need more work.

--- cons.rb
+++ cons.rb
@@ -7,45 +7,69 @@ class CONS
     #strip headings
     raw_array = data.split(/>Rosalind_[0-9].*?\n/)
 
-    processed_array = []
+    results_array = []
 
-    for i in 0..raw_array[0].length
-      for j in 0...raw_array.length
-        puts raw_array[j][i]
-      end
-      puts "\n"
+    for i in 0...raw_array[1].length
+      results_array << ""
     end
-
-
-
 
     for i in 0...raw_array.length
-      processed_string = ""
       for j in 0...raw_array[i].length
-        processed_string = processed_string + raw_array[i][j]
+
+        case raw_array[i][j]
+          when 'A'
+            results_array[j] += 'A'
+          when 'G'
+            results_array[j] += 'G'
+          when 'C'
+            results_array[j] += 'C'
+          when 'T'
+            results_array[j] += 'T'
+          when nil
+            #do nothing
+          else
+            puts raw_array[i][j]
+        end
       end
-      puts processed_string
     end
 
-    #iterate through taking first char, then second, etc to build new string
-    #build string each time into new array
+    puts results_array.join(" ")
 
-    #cycle through new array and count each type
+    a_string_ret = ""
+    c_string_ret = ""
+    g_string_ret = ""
+    t_string_ret = ""
 
+    ret_string = ""
 
+    for i in 0...results_array.length - 1
 
+      a_count = results_array[i].count('A')
+      c_count = results_array[i].count('C')
+      g_count = results_array[i].count('G')
+      t_count = results_array[i].count('T')
 
+      if a_count > c_count and a_count > g_count and a_count > t_count
+        ret_string += "A"
+      elsif c_count > a_count and c_count > g_count and c_count > t_count
+        ret_string += "C"
+      elsif g_count > a_count and g_count > c_count and g_count > t_count
+        ret_string += "G"
+      elsif t_count > a_count and t_count > g_count and t_count > c_count
+        ret_string += "T"
+      end
 
+    a_string_ret += a_count.to_s + " "
+    c_string_ret += c_count.to_s + " "
+    g_string_ret += g_count.to_s + " "
+    t_string_ret += t_count.to_s + " "
+    end
 
-
-
+    puts ret_string
+    puts "A: " + a_string_ret
+    puts "C: " + c_string_ret
+    puts "G: " + g_string_ret
+    puts "T: " + t_string_ret
   end
-
-
-
-
-
-
-
 
 end
