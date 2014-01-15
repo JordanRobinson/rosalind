13cc1cafa4aef2ce9d9e9e6e7bf88f5dd1f534c5
Jordan Robinson <hypahnova@gmail.com>
2014-01-15 18:45:50 +0000

    Minor fix for cons, also adds comments.

--- cons.rb
+++ cons.rb
@@ -13,7 +13,7 @@ class CONS
       results_array << ""
     }
 
-    for i in 0...raw_array.length
+    for i in 0...raw_array.length #make a big easily countable array of strings
       for j in 0...raw_array[i].length
 
         case raw_array[i][j]
@@ -33,8 +33,6 @@ class CONS
       end
     end
 
-    puts results_array.join(" ")
-
     a_string_ret = ""
     c_string_ret = ""
     g_string_ret = ""
@@ -44,12 +42,12 @@ class CONS
 
     for i in 0...results_array.length - 1
 
-      a_count = results_array[i].count('A')
+      a_count = results_array[i].count('A') #count each possibility
       c_count = results_array[i].count('C')
       g_count = results_array[i].count('G')
       t_count = results_array[i].count('T')
 
-      if a_count > c_count and a_count > g_count and a_count > t_count
+      if a_count > c_count and a_count > g_count and a_count > t_count #build up a pretty string for the heading
         ret_string += "A"
       elsif c_count > a_count and c_count > g_count and c_count > t_count
         ret_string += "C"
@@ -59,14 +57,14 @@ class CONS
         ret_string += "T"
       end
 
-    a_string_ret += a_count.to_s + " "
+    a_string_ret += a_count.to_s + " " #and then construct the matrix
     c_string_ret += c_count.to_s + " "
     g_string_ret += g_count.to_s + " "
     t_string_ret += t_count.to_s + " "
     end
 
     puts ret_string
-    puts "A: " + a_string_ret
+    puts "A: " + a_string_ret #finally, print it out so it's as it should look
     puts "C: " + c_string_ret
     puts "G: " + g_string_ret
     puts "T: " + t_string_ret
