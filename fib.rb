4ccf6f4db5ef71f3d282c9b468a6c11c4d64e644
Jordan Robinson <hypahnova@gmail.com>
2014-01-18 16:14:48 +0000

    Adds solution for FIB.

--- fib.rb
+++ fib.rb
@@ -2,5 +2,17 @@ class FIB
 #Rosalind exercise: Rabbits and Recurrence Relations
 
   def run(data)
+
+    raw_array = data.split(" ")
+    puts recurse(Integer(raw_array[0]), Integer(raw_array[1]))
   end
+
+  def recurse(x, y)
+    if x == 1 or x == 2
+      1
+    else
+      recurse(x-1, y) + recurse(x-2, y) * y
+    end
+  end
+
 end
