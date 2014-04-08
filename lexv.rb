bcb54bd487140d603b5b98850e19c4aba375d2cb
Jordan Robinson <hypahnova@gmail.com>
2014-04-08 23:11:51 +0100

    Adds LEXV, some small style changes, adds data files.
    The data files are the files used for each exercise, so it's pretty easy now to confirm how each exercise works.
    
    LEXV has been added, and has taken LEXF as a template, but still requires some work in terms of ordering.
    
    There has also been some work towards greater portability of the solution in its entirety.

---
+++ lexv.rb
@@ -0,0 +1,45 @@
+class LEXV
+
+  #Rosalind exercise: Ordering Strings of Varying Length Lexicographically
+
+  def run(data)
+
+    raw_array = data.split(/\n/)
+
+    letters = raw_array[0].split(/ /)
+
+    length = Integer(raw_array[1]) #get the word length from the split text file
+
+    #iterate through and add a copy of each character so that they can be applied to each word x amount of times
+    #where x is the length earlier specified
+    temp_letters = []
+
+    for i in 0..letters.length
+      for j in 0...length
+        temp_letters << letters[i]
+      end
+      temp_letters
+    end
+
+    letters = temp_letters
+
+    #get all possible permutations of length x of the array, that contains x copies of all the letters available
+    letters = letters.permutation(length).to_a
+
+    output = []
+
+    for i in 0..letters.length
+      unless letters[i].nil? #or letters[i].join("").length < length #weed out words that are too small
+        output << letters[i].join("")[0...length]
+      end
+    end
+
+    output = output.uniq #remove any duplicates since permutation doesn't
+
+    output.each do |i| #print everything out
+      puts i
+    end
+
+
+  end
+end
--- rosalind.rb
+++ rosalind.rb
@@ -1,30 +1,31 @@
 class Rosalind
 
-  $exercise_name = "lcsq"
+  $exercise_name = 'lexv'
 
   require 'time'
-  require "./" + $exercise_name + ".rb"
+  require './' + $exercise_name + '.rb'
 
   $data
-  $path = "D:\\0\\rosalind\\"
+  $path = '/data/'
 
   def load_data(file_name)
-    full_path = $path.concat(file_name)
-    $data = File.open(full_path).read
+    $data = File.open(Dir.pwd + $path + file_name).read
+
   end
 
   def run(exercise_name)
-    #xerciseOne.new.run($data)
-  start = Time.now
-  eval($exercise_name.to_s.upcase).new.run($data)
-  elapsed = (Time.now - start)
-  print "\nExercise " + $exercise_name + " completed in " + elapsed.to_s + " seconds"
+    #exerciseOne.new.run($data)
+    start = Time.now
+    print Dir.pwd
+    eval($exercise_name.to_s.upcase).new.run($data)
+    elapsed = (Time.now - start)
+    print '\nExercise ' + $exercise_name + ' completed in ' + elapsed.to_s + ' seconds'
   end
 end
 
 
 runner = Rosalind.new
-runner.load_data($exercise_name + ".txt")
+runner.load_data($exercise_name + '.txt')
 runner.run($exercise_name)
 
 
