9064f4a55e170109e1b7856e9eef0cf6555b18d2
Jordan Robinson <hypahnova@gmail.com>
2014-06-10 21:02:23 +0100

    Starts work on KMER.

--- kmer.rb
+++ kmer.rb
@@ -7,6 +7,31 @@ class KMER
     data.gsub!(/[0-9]./, '')
     data_array = data.split(/>Rosalind_/)
 
+    puts "\n" + data_array[1]
+
+    rough_kmer_alphabet = %w[A A A A C C C C G G G G T T T T].permutation(4).to_a.sort
+
+    kmer_alphabet = []
+
+    rough_kmer_alphabet.each { |x|
+      kmer_alphabet << x.join
+    }
+
+    i = 0
+    kmer_alphabet.uniq.each { |x|
+      puts x
+
+
+
+
+
+
+      puts data_array[1].split(//)
+
+
+      i = i + 1
+    }
+
   end
 
 end
