33c75e8197d347f0dd9fea2da72b32cc681fe328
Jordan Robinson <hypahnova@gmail.com>
2014-06-19 08:03:36 +0100

    More work on test case setup

--- src/dna.rb
+++ src/dna.rb
@@ -1,8 +1,7 @@
 class DNA
 
   def run(data)
-
-    puts count_char(data, "A").to_s +
+    count_char(data, "A").to_s +
              " " + count_char(data, "C").to_s +
              " " + count_char(data, "G").to_s +
              " " + count_char(data, "T").to_s
